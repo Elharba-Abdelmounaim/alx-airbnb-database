2b64f7260c77be2b458fa63d9d0ab0d0ce594312
ABDELMOUNAIM <elharba.abdelmounaim@gmail.com>
2025-05-19 03:15:24 +0100

    Update aggregations_and_window_functions.sql
    sa

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -5,17 +5,13 @@ FROM
     bookings
 GROUP BY 
     user_id
-ORDER BY 
-    total_bookings DESC;
+
 --
 
-SELECT 
-    property_id,
-    COUNT(*) AS total_bookings,
-    RANK() OVER (ORDER BY COUNT(*) DESC) AS booking_rank
-FROM 
-    bookings
-GROUP BY 
-    property_id
-ORDER BY 
-    booking_rank;
+SELECT property_id, total_bookings,
+       ROW_NUMBER() OVER (ORDER BY total_bookings DESC) AS booking_rank
+FROM (
+    SELECT property_id, COUNT(*) AS total_bookings
+    FROM bookings
+    GROUP BY property_id
+) AS property_bookings;
