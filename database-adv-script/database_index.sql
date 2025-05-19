29801b41897497cd122de76966d7b2ca19f63eb7
ABDELMOUNAIM <elharba.abdelmounaim@gmail.com>
2025-05-19 03:20:53 +0100

    Update database_index.sql
    h

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -1,11 +1,10 @@
--- Create index on user_id in bookings table to optimize JOIN and WHERE queries
+-- Measure performance before adding indexes
+EXPLAIN ANALYZE
+SELECT * FROM bookings WHERE user_id = 123;
+
+-- Create indexes to improve performance
 CREATE INDEX idx_bookings_user_id ON bookings(user_id);
 
--- Create index on property_id in bookings table to speed up property-related queries
-CREATE INDEX idx_bookings_property_id ON bookings(property_id);
-
--- Create index on email in users table for faster lookups by email
-CREATE INDEX idx_users_email ON users(email);
-
--- Create index on city in properties table to improve search performance by city
-CREATE INDEX idx_properties_city ON properties(city);
+-- Measure performance after adding indexes
+EXPLAIN ANALYZE
+SELECT * FROM bookings WHERE user_id = 123;
