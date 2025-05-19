555e39fd0b2eb5eb8e004c4a176eb72fd48e3ec3
ABDELMOUNAIM <elharba.abdelmounaim@gmail.com>
2025-05-19 02:41:48 +0100

    Update joins_queries.sql
    s

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,30 +1,39 @@
 -- 1. INNER JOIN to get bookings and respective users
-SELECT 
+SELECT
     bookings.id AS booking_id,
+    bookings.property_id,
+    bookings.booking_date,
     users.id AS user_id,
     users.name AS user_name,
-    bookings.property_id,
-    bookings.start_date,
-    bookings.end_date
-FROM bookings
-INNER JOIN users ON bookings.user_id = users.id;
+    users.email
+FROM
+    bookings
+INNER JOIN
+    users ON bookings.user_id = users.id;
+
 
 -- 2. LEFT JOIN to get properties and their reviews (even if no reviews)
-SELECT 
+SELECT
     properties.id AS property_id,
     properties.name AS property_name,
     reviews.id AS review_id,
-    reviews.comment,
-    reviews.rating
-FROM properties
-LEFT JOIN reviews ON reviews.property_id = properties.id;
+    reviews.rating,
+    reviews.comment
+FROM
+    properties
+LEFT JOIN
+    reviews ON properties.id = reviews.property_id;
+
 
 -- 3. FULL OUTER JOIN to get all users and all bookings (linked or not)
-SELECT 
+SELECT
     users.id AS user_id,
     users.name AS user_name,
     bookings.id AS booking_id,
     bookings.property_id,
-    bookings.start_date
-FROM users
-FULL OUTER JOIN bookings ON users.id = bookings.user_id;
+    bookings.booking_date
+FROM
+    users
+FULL OUTER JOIN
+    bookings ON users.id = bookings.user_id;
+
